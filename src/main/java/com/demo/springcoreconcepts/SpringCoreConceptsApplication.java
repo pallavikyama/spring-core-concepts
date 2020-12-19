81017d604944c655851273481ab9fa5305a661d2
pallavikyama <kyamap98@gmail.com>
2020-12-19 21:38:02 +0530

    Added: Used the in-built Simple Logging Framework (SLF4J) of Spring boot to log all messages.

--- src/main/java/com/demo/springcoreconcepts/SpringCoreConceptsApplication.java
+++ src/main/java/com/demo/springcoreconcepts/SpringCoreConceptsApplication.java
@@ -2,17 +2,20 @@ package com.demo.springcoreconcepts;
 
 import com.demo.springcoreconcepts.component.DemoBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreConceptsApplication {
+	public static final Logger logger=LoggerFactory.getLogger(SpringCoreConceptsApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Core Concepts Demo!");
+		logger.debug("Welcome to Spring Core Concepts!");
 		ApplicationContext context = SpringApplication.run(SpringCoreConceptsApplication.class, args);
-		System.out.println("Checking Context: " + context.getBean(DemoBean.class));
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
 	}
 
 }
